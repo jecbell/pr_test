3a0f2dfdf7692742463d3499ef3d8d7a219861ca
jecbell <jecbell@gmail.com>
2023-03-15 13:26:55 +0000

    add teams field to user model

--- pr_test/users/models.py
+++ pr_test/users/models.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField
+from django.db.models import CharField, ManyToManyField
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 
@@ -15,6 +15,7 @@ class User(AbstractUser):
     name = CharField(_("Name of User"), blank=True, max_length=255)
     first_name = None  # type: ignore
     last_name = None  # type: ignore
+    teams = ManyToManyField("teams.Team", blank=True, related_name="users")
 
     def get_absolute_url(self):
         """Get url for user's detail view.
