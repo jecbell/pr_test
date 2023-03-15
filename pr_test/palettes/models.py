3da1a86e89e6dc94ce302e91e577bca00b0914c2
jecbell <jecbell@gmail.com>
2023-03-15 12:32:54 +0000

    add foreign key to user

--- pr_test/palettes/models.py
+++ pr_test/palettes/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
@@ -5,6 +6,9 @@ from django.db import models
 class Palette(models.Model):
     name = models.CharField(max_length=255)
     colors = ArrayField(models.CharField(max_length=255))
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="palettes"
+    )
 
     def __str__(self):
         return self.name
